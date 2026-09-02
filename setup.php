0a899b13aa063f146706559e7d2fc0441169e9b2
Leonardo Poiatti Fação <asasdeltasevida@gmail.com>
2026-09-02 14:33:56 -0300

    chore: 1.5.3 - consolida fix

--- setup.php
+++ setup.php
@@ -6,7 +6,7 @@
  * License: GPLv2+
  */
 
-define('PLUGIN_PROTOCOLO_VERSION', '1.5.2');
+define('PLUGIN_PROTOCOLO_VERSION', '1.5.3');
 define('PLUGIN_PROTOCOLO_MIN_GLPI', '11.0.0');
 define('PLUGIN_PROTOCOLO_MAX_GLPI', '12.0.0');
 define('PLUGIN_PROTOCOLO_NAMESPACE', 'GlpiPlugin\\Protocolo');
